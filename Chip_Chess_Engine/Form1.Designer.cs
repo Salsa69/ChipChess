a6c263970b244e7074d24d7f89cd69885721e1a6
Mitch <60634216+Salsa69@users.noreply.github.com>
2021-03-31 18:52:08 -0700

    Innitial
    A very nice win form with an efficient method of drawing the chess board

---
+++ Chip_Chess_Engine/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Chip_Chess_Engine
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.exit = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.min = new System.Windows.Forms.Button();
+            this.fullScrn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // exit
+            // 
+            this.exit.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (25)))), ((int) (((byte) (25)))), ((int) (((byte) (25)))));
+            this.exit.Dock = System.Windows.Forms.DockStyle.Right;
+            this.exit.FlatAppearance.BorderSize = 0;
+            this.exit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (255)))), ((int) (((byte) (103)))), ((int) (((byte) (92)))));
+            this.exit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (255)))), ((int) (((byte) (103)))), ((int) (((byte) (92)))));
+            this.exit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.exit.ForeColor = System.Drawing.Color.White;
+            this.exit.Location = new System.Drawing.Point(574, 0);
+            this.exit.Name = "exit";
+            this.exit.Size = new System.Drawing.Size(26, 23);
+            this.exit.TabIndex = 0;
+            this.exit.Text = "✕";
+            this.exit.UseVisualStyleBackColor = false;
+            this.exit.Click += new System.EventHandler(this.exit_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (25)))), ((int) (((byte) (25)))), ((int) (((byte) (25)))));
+            this.panel1.Controls.Add(this.min);
+            this.panel1.Controls.Add(this.fullScrn);
+            this.panel1.Controls.Add(this.exit);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 23);
+            this.panel1.TabIndex = 1;
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.panel1_MouseUp);
+            // 
+            // min
+            // 
+            this.min.Dock = System.Windows.Forms.DockStyle.Right;
+            this.min.FlatAppearance.BorderSize = 0;
+            this.min.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (39)))), ((int) (((byte) (41)))), ((int) (((byte) (43)))));
+            this.min.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (39)))), ((int) (((byte) (41)))), ((int) (((byte) (43)))));
+            this.min.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.min.ForeColor = System.Drawing.Color.White;
+            this.min.Location = new System.Drawing.Point(522, 0);
+            this.min.Name = "min";
+            this.min.Size = new System.Drawing.Size(26, 23);
+            this.min.TabIndex = 1;
+            this.min.Text = "—";
+            this.min.UseVisualStyleBackColor = true;
+            this.min.Click += new System.EventHandler(this.min_Click);
+            // 
+            // fullScrn
+            // 
+            this.fullScrn.Dock = System.Windows.Forms.DockStyle.Right;
+            this.fullScrn.FlatAppearance.BorderSize = 0;
+            this.fullScrn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (39)))), ((int) (((byte) (41)))), ((int) (((byte) (43)))));
+            this.fullScrn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (39)))), ((int) (((byte) (41)))), ((int) (((byte) (43)))));
+            this.fullScrn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.fullScrn.ForeColor = System.Drawing.Color.White;
+            this.fullScrn.Location = new System.Drawing.Point(548, 0);
+            this.fullScrn.Name = "fullScrn";
+            this.fullScrn.Size = new System.Drawing.Size(26, 23);
+            this.fullScrn.TabIndex = 2;
+            this.fullScrn.Text = "❐";
+            this.fullScrn.UseVisualStyleBackColor = true;
+            this.fullScrn.Click += new System.EventHandler(this.fullScrn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (31)))), ((int) (((byte) (31)))), ((int) (((byte) (31)))));
+            this.ClientSize = new System.Drawing.Size(600, 440);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+        }
+
+        private System.Windows.Forms.Button min;
+        private System.Windows.Forms.Button fullScrn;
+
+        private System.Windows.Forms.Panel panel1;
+
+        private System.Windows.Forms.Button exit;
+
+        #endregion
+    }
+}
---
+++ Chip_Chess_Engine/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+
+public struct Piece
+{
+    public ushort ID;
+    public int X;
+    public int Y;
+    public string Name;
+}
+
+namespace Chip_Chess_Engine
+{
+    public partial class Form1 : Form
+    {
+        private readonly SolidBrush _brush1 = new(Color.LightSalmon);
+        private readonly SolidBrush _brush2 = new(Color.Chocolate);
+        private Graphics _graphics;
+        private int _boardSize = 50;
+        
+        private bool _dragging;
+        private Point _dragCursorPoint;
+        private Point _dragFormPoint;
+
+        private Rectangle[/* y coord */][/* x coord */] _squares;
+        
+        #region Window Move
+        
+        private void panel1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if(WindowState == FormWindowState.Maximized) {
+                WindowState = FormWindowState.Normal;
+                Location = new Point(Left, 10);
+                Cursor.Position = MousePosition.X < Left ? new Point(Location.X + 10, 15) : new Point(MousePosition.X, 15);
+                _boardSize = 50;
+                Refresh();
+            }
+            _dragging = true;
+            _dragCursorPoint = Cursor.Position;
+            _dragFormPoint = this.Location;
+        }
+        private void panel1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging) {
+                Point dif = Point.Subtract(Cursor.Position, new Size(_dragCursorPoint));
+                Location = Point.Add(_dragFormPoint, new Size(dif));
+                if (MousePosition.Y == 0) {
+                    WindowState = FormWindowState.Maximized;
+                    _boardSize = 80;
+                    Refresh();
+                }
+            }
+        }
+        private void panel1_MouseUp(object sender, MouseEventArgs e) => _dragging = false;
+        
+        #endregion
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            BuildGrid();
+        }
+        
+        private void BuildGrid()
+        {
+            for (int y = 0; y < 8; y++)
+            {
+                for (int x = 0; x < 8; x++)
+                {
+                    _graphics = this.CreateGraphics();
+                    _graphics.FillRectangle(_brush1, new Rectangle(x * _boardSize + 40, y * _boardSize + 70, _boardSize, _boardSize));
+                    _squares[y][x] = new Rectangle(x * _boardSize + 40, y * _boardSize + 70, _boardSize, _boardSize);
+                    
+                    x++;
+                    
+                    _graphics = this.CreateGraphics();
+                    _graphics.FillRectangle(_brush2, new Rectangle(x * _boardSize + 40, y * _boardSize + 70, _boardSize, _boardSize));
+                    _squares[y][x] = new Rectangle(x * _boardSize + 40, y * _boardSize + 70, _boardSize, _boardSize);
+                    
+                }
+
+                y++;
+                
+                for (int x = 0; x < 8; x++)
+                {
+                    _graphics = this.CreateGraphics();
+                    _graphics.FillRectangle(_brush2, new Rectangle(x * _boardSize + 40, y * _boardSize + 70, _boardSize, _boardSize));
+
+                    x++;
+                    
+                    _graphics = this.CreateGraphics();
+                    _graphics.FillRectangle(_brush1, new Rectangle(x * _boardSize + 40, y * _boardSize + 70, _boardSize, _boardSize));
+                    
+                }
+            }
+        }
+
+        private void PlacePieces()
+        {
+            
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void fullScrn_Click(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Maximized) {
+                WindowState = FormWindowState.Normal;
+                _boardSize = 50;
+            }
+            else if (WindowState == FormWindowState.Normal) {
+                WindowState = FormWindowState.Maximized;
+                _boardSize = 80;
+            }
+            Refresh();
+        }
+
+        private void min_Click(object sender, EventArgs e) => WindowState = FormWindowState.Minimized;
+    }
+}
